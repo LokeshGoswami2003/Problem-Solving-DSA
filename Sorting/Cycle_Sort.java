af9917f3c17e62cff53e0ee8bef9caf9e6b3a52e
LokeshGoswami2003 <lokesh.goswami.2003@gmail.com>
2022-03-20 13:43:16 +0530

    Patterns added

---
+++ Sorting/Cycle_Sort.java
@@ -0,0 +1,13 @@
+class Sorting{
+    public int[] sort(int[] arr){
+        return new int[]{-1,-1};
+    }
+}
+public class Cycle_Sort {
+    public static void main(String[] args) {
+        int[] arr={3,5,2,1,4};
+        Sorting ss=new Sorting();
+        int[] newarr=ss.sort(arr);
+        
+    }
+}
