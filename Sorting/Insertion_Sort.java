547f966468e75b4610ed75f2c85e8ae8e937f2c0
LokeshGoswami2003 <lokesh.goswami.2003@gmail.com>
2022-03-24 14:38:23 +0530

    insertion sort with for loops

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -1,34 +1,37 @@
-import java.util.Arrays;
-
-public class Insertion_Sort {
+public class Insertion_Sort{
     public static void main(String[] args) {
-        int[] arr={5,9,8,6,1};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
-
+        int[] arr={10,9,8,7,6,5,4,3,2};
+        Insertion ss=new Insertion();
+        int[] na=ss.sort(arr);
+        ss.print(na);
     }
+}
 
-    static void sort(int[] arr){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j>0;j--){
-                if(arr[j-1]>arr[j]){
-                    swap(arr,j-1,j);
-                }
-                else
-                {
+class Insertion{
+    public int[] sort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int k=i;
+            for(int j=i-1;j>=0;j--){
+                if(arr[k]>=arr[j]){
                     break;
+                }
+                if(arr[k]<arr[j]){
+                    
+                    swap(arr,k,j);
+                    k--;
                 }
             }
         }
+        return arr;
     }
 
-static void swap(int[] arr,int a,int b){
+    public  void swap(int[] arr,int a,int b){
         int temp=arr[a];
         arr[a]=arr[b];
         arr[b]=temp;
     }
-
-    static void printing(int[] arr){
+    
+    public void print(int[] arr){
         for(int k=0;k<arr.length;k++)
         {
             System.out.println(arr[k]);
