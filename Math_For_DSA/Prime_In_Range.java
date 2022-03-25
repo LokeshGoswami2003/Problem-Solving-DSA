6509b248b3291a4da20a1f8ce62033266582d78f
LokeshGoswami2003 <lokesh.goswami.2003@gmail.com>
2022-03-25 08:35:49 +0530

    cycle sort added

---
+++ Math_For_DSA/Prime_In_Range.java
@@ -0,0 +1,11 @@
+public class Prime_In_Range{
+    public static void main(String[] args) {
+        int range=40;
+        primein(range);
+    }
+    static void primein(int range){
+        for(int i=2;i<n;i++){
+            for(int j=2;j<i;)
+        }
+    }
+}
--- Sorting/Cycle_Sort.java
+++ Sorting/Cycle_Sort.java
@@ -1,13 +1,32 @@
-class Sorting{
-    public int[] sort(int[] arr){
-        return new int[]{-1,-1};
-    }
-}
-public class Cycle_Sort {
+public class Cycle_Sort { // given numbers in range 1 to n
     public static void main(String[] args) {
-        int[] arr={3,5,2,1,4};
-        Sorting ss=new Sorting();
-        int[] newarr=ss.sort(arr);
-        
+        int[] arr = { 1, 2, 5, 4, 3 };
+        cycle(arr);
+        print(arr);
+    }
+
+    static void cycle(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int corrrect = arr[i] - 1;
+            if (arr[i] != arr[corrrect]) {
+                swap(arr, i, corrrect);
+            } else {
+                i++;
+            }
+        }
+
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    static void print(int[] arr) {
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }
     }
 }
